2ccb63af7988b163c59b7a812e8b3c154c6b55e8
dimas2jc <50562377+dimas2jc@users.noreply.github.com>
2020-10-25 22:31:40 +0700

    first init

---
+++ resources/views/admin/acara.blade.php
@@ -0,0 +1,53 @@
+@section('title', 'Acara') 
+@extends('layouts.main')
+@section('style')
+<!-- DataTables css -->
+<!-- <link href="{{ asset('assets/plugins/datatables/dataTables.bootstrap4.min.css') }}" rel="stylesheet" type="text/css" /> -->
+<!-- <link href="{{ asset('assets/plugins/datatables/buttons.bootstrap4.min.css') }}" rel="stylesheet" type="text/css" /> -->
+<!-- Responsive Datatable css -->
+<!-- <link href="{{ asset('assets/plugins/datatables/responsive.bootstrap4.min.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('assets/plugins/sweet-alert2/sweetalert2.min.css') }}" rel="stylesheet" type="text/css" />
+<script src="{{ asset('assets/plugins/sweet-alert2/sweetalert2.min.js') }}"></script> -->
+@endsection 
+@section('rightbar-content')
+<!-- Start Breadcrumbbar -->                    
+<div class="breadcrumbbar">
+                <div class="row align-items-center">
+                    <div class="col-md-8 col-lg-8">
+                        <h4 class="page-title">Buat Acara</h4>
+                        <div class="breadcrumb-list">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="index.html">Acara</a></li>
+                                <li class="breadcrumb-item active" aria-current="page">Buat Acara</li>
+                            </ol>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-lg-4">
+                        <!-- <div class="widgetbar">
+                            <button class="btn btn-primary-rgba"><i class="feather icon-plus mr-2"></i>Actions</button>
+                        </div>                         -->
+                    </div>
+                </div>          
+            </div>
+            <!-- End Breadcrumbbar -->
+            <!-- Start Contentbar -->    
+            <div class="contentbar">                
+                <!-- Start row -->
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-body">
+                        
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- End row -->
+            </div>
+            <!-- End Contentbar -->
+@endsection 
+@section('script')
+<!-- Datatable js -->
+<!-- <script src="{{ asset('assets/plugins/datatables/jquery.dataTables.min.js') }}"></script> -->
+<!-- <script src="{{ asset('assets/plugins/datatables/dataTables.bootstrap4.min.js') }}"></script> -->
+@endsection 
---
+++ resources/views/user-login.blade.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="keywords" content="e-sertif, sertifikat vokasi unair">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Login - E-Sertifikat Fakultas Vokasi</title>
+
+    <!-- Fevicon -->
+    <link rel="shortcut icon" href="{{ asset('assets/images/LogoUnair.png') }}">
+    <!-- Start css -->
+    <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet" type="text/css">
+    <style>
+        .card{
+            box-shadow: 10px 10px 15px;
+        }
+    </style>
+    <!-- End css -->
+</head>
+<body class="vertical-layout">
+    <!-- Start Containerbar -->
+    <div id="containerbar" class="containerbar">
+        <!-- Start Container -->
+        <div class="container">
+            <div class="auth-box login-box">
+                <!-- Start row -->
+                <div class="row no-gutters align-items-center justify-content-center">
+                    <!-- Start col -->
+                    <div class="col-md-6 col-lg-5">
+                        <!-- Start Auth Box -->
+                        <div class="auth-box-right">
+                            <div class="card">
+                                <div class="card-body">
+                                    <form action="{{ url('/auth') }}">
+                                        <div class="form-head">
+                                            <h1>E-Sertifikat Fakultas Vokasi</h1>
+                                        </div>                                        
+                                        <h4 class="text-primary my-4">Log in !</h4>
+                                        <div class="form-group">
+                                            <input type="text" class="form-control" id="username" placeholder="Enter Username here" required>
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="password" class="form-control" id="password" placeholder="Enter Password here" required>
+                                        </div>
+                                        <div class="form-row mb-3">
+                                            <div class="col-sm-6">
+                                                <div class="custom-control custom-checkbox text-left">
+                                                  <input type="checkbox" class="custom-control-input" id="rememberme">
+                                                  <label class="custom-control-label font-14" for="rememberme">Remember Me</label>
+                                                </div>                                
+                                            </div>
+                                            <div class="col-sm-6">
+                                              <div class="forgot-psw"> 
+                                                <a id="forgot-psw" href="user-forgotpsw.html" class="font-14">Forgot Password?</a>
+                                              </div>
+                                            </div>
+                                        </div>                          
+                                      <button type="submit" class="btn btn-success btn-lg btn-block font-18">Log in</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- End Auth Box -->
+                    </div>
+                    <!-- End col -->
+                </div>
+                <!-- End row -->
+            </div>
+        </div>
+        <!-- End Container -->
+    </div>
+    <!-- End Containerbar -->
+    <!-- Start js -->        
+    <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
+    <script src="{{ asset('assets/js/popper.min.js') }}"></script>
+    <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('assets/js/modernizr.min.js') }}"></script>
+    <script src="{{ asset('assets/js/detect.js') }}"></script>
+    <script src="{{ asset('assets/js/jquery.slimscroll.js') }}"></script>
+    <!-- End js -->
+</body>
+</html>
