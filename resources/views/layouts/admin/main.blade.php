409fe254a61101b9feb77a2f2c1382ec2f15323a
dimas2jc <50562377+dimas2jc@users.noreply.github.com>
2020-10-29 23:31:05 +0700

    login selesai

--- resources/views/layouts/admin/main.blade.php
+++ resources/views/layouts/admin/main.blade.php
@@ -1,128 +1,50 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="keywords" content="e-sertif, sertifikat vokasi unair">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>@yield('title')</title>
-
-    <!-- Fevicon -->
-    <link rel="shortcut icon" href="{{ asset('assets/images/LogoUnair.png') }}">
-
-    <!-- Start CSS -->   
+<html lang="id">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="keywords" content="e-sertif, sertifikat vokasi unair">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title> @yield('title') </title>
+        <!-- Favicon -->
+        <link rel="shortcut icon" href="{{ asset('assets/images/LogoUnair.png') }}">
+        <!-- Start CSS -->   
         @yield('style')
         <link href="{{ asset('assets/plugins/switchery/switchery.min.css') }}" rel="stylesheet" type="text/css">
         <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
         <link href="{{ asset('assets/css/icons.css') }}" rel="stylesheet" type="text/css">
         <link href="{{ asset('assets/css/flag-icon.min.css') }}" rel="stylesheet" type="text/css">
         <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet" type="text/css">
-    <!-- End CSS -->
-</head>
-<body class="vertical-layout">    
-    <!-- Start Infobar Setting Sidebar -->
-    <div id="infobar-settings-sidebar" class="infobar-settings-sidebar">
-        <div class="infobar-settings-sidebar-head d-flex w-100 justify-content-between">
-            <h4>Settings</h4><a href="javascript:void(0)" id="infobar-settings-close" class="infobar-settings-close"><img src="assets/images/svg-icon/close.svg" class="img-fluid menu-hamburger-close" alt="close"></a>
-        </div>
-        <div class="infobar-settings-sidebar-body">
-            <div class="custom-mode-setting">
-                <div class="row align-items-center pb-3">
-                    <div class="col-8"><h6 class="mb-0">New Order Notification</h6></div>
-                    <div class="col-4 text-right"><input type="checkbox" class="js-switch-setting-first" checked /></div>
-                </div>
-                <div class="row align-items-center pb-3">
-                    <div class="col-8"><h6 class="mb-0">Low Stock Alerts</h6></div>
-                    <div class="col-4 text-right"><input type="checkbox" class="js-switch-setting-second" checked /></div>
-                </div>
-                <div class="row align-items-center pb-3">
-                    <div class="col-8"><h6 class="mb-0">Vacation Mode</h6></div>
-                    <div class="col-4 text-right"><input type="checkbox" class="js-switch-setting-third" /></div>
-                </div>
-                <div class="row align-items-center pb-3">
-                    <div class="col-8"><h6 class="mb-0">Order Tracking</h6></div>
-                    <div class="col-4 text-right"><input type="checkbox" class="js-switch-setting-fourth" checked /></div>
-                </div>
-                <div class="row align-items-center pb-3">
-                    <div class="col-8"><h6 class="mb-0">Newsletter Subscription</h6></div>
-                    <div class="col-4 text-right"><input type="checkbox" class="js-switch-setting-fifth" checked /></div>
-                </div>
-                <div class="row align-items-center pb-3">
-                    <div class="col-8"><h6 class="mb-0">Show Review</h6></div>
-                    <div class="col-4 text-right"><input type="checkbox" class="js-switch-setting-sixth" /></div>
-                </div>
-                <div class="row align-items-center pb-3">
-                    <div class="col-8"><h6 class="mb-0">Enable Wallet</h6></div>
-                    <div class="col-4 text-right"><input type="checkbox" class="js-switch-setting-seventh" checked /></div>
-                </div>
-                <div class="row align-items-center">
-                    <div class="col-8"><h6 class="mb-0">Sales Report</h6></div>
-                    <div class="col-4 text-right"><input type="checkbox" class="js-switch-setting-eightth" checked /></div>
-                </div>
-            </div>
-            <div class="custom-layout-setting">
-                <div class="row align-items-center pb-3">
-                    <div class="col-12">
-                        <h6 class="mb-3">Select Account</h6>
-                    </div>
-                    <div class="col-6">
-                        <div class="account-box active">
-                            <img src="assets/images/users/boy.svg" class="img-fluid" alt="user">
-                            <h5>John</h5>
-                            <p>CEO</p>
-                        </div>                        
-                    </div>
-                    <div class="col-6">
-                        <div class="account-box">
-                            <img src="assets/images/users/women.svg" class="img-fluid" alt="user">
-                            <h5>Kate</h5>
-                            <p>COO</p>
-                        </div>
-                    </div>
-                    <div class="col-6">
-                        <div class="account-box">
-                            <img src="assets/images/users/men.svg" class="img-fluid" alt="user">
-                            <h5>Mark</h5>
-                            <p>MD</p>
-                        </div>                        
-                    </div>
-                    <div class="col-6">
-                        <div class="account-box">
-                            <p class="dash-analytic-icon"><i class="feather icon-plus font-35"></i></p>
-                            <h5>Add</h5>
-                            <p>ACCOUNT</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="infobar-settings-sidebar-overlay"></div>
-    <!-- End Infobar Setting Sidebar -->
-    <!-- Start Containerbar -->
-    <div id="containerbar">
-        <!-- Start Leftbar -->
-            @include('layouts.admin.leftbar')
-        <!-- End Leftbar -->
-        <!-- Start Rightbar -->
+
+        <!-- FontAwesome css -->
+        <link rel="stylesheet" href="{{ asset('assets/fontawesome/css/all.min.css') }}">
+        
+        <!-- End CSS -->
+    </head>
+    <body class="vertical-layout">
+        <!-- Start Containerbar -->
+        <div id="containerbar">     
+            <!-- Start Leftbar -->
+            @include('layouts.admin.leftbar') 
+            <!-- End Leftbar -->
+            <!-- Start Rightbar -->
             @include('layouts.admin.rightbar')          
             @yield('content')
-        <!-- End Rightbar -->
-    </div>
-    <!-- End Containerbar -->
-
-    <!-- Start js -->        
-    <script src="assets/js/jquery.min.js"></script>
-    <script src="assets/js/popper.min.js"></script>
-    <script src="assets/js/bootstrap.min.js"></script>
-    <script src="assets/js/modernizr.min.js"></script>
-    <script src="assets/js/detect.js"></script>
-    <script src="assets/js/jquery.slimscroll.js"></script>
-    <script src="assets/js/vertical-menu.js"></script>
-    <!-- Switchery js -->
-    <script src="assets/plugins/switchery/switchery.min.js"></script>
-    <!-- Core js -->
-    <script src="assets/js/core.js"></script>
-    <!-- End js -->
-</body>
-</html>
+            <!-- End Rightbar --> 
+        </div>
+        <!-- End Containerbar -->
+        <!-- Start JS -->        
+        <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
+        <script src="{{ asset('assets/js/popper.min.js') }}"></script>
+        <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+        <script src="{{ asset('assets/js/modernizr.min.js') }}"></script>
+        <script src="{{ asset('assets/js/detect.js') }}"></script>
+        <script src="{{ asset('assets/js/jquery.slimscroll.js') }}"></script>
+        <script src="{{ asset('assets/js/vertical-menu.js') }}"></script> 
+        <script src="{{ asset('assets/plugins/switchery/switchery.min.js') }}"></script> 
+        @yield('script')
+        <!-- Core JS -->
+        <script src="{{ asset('assets/js/core.js') }}"></script>
+        <!-- End JS -->
+    </body>
+</html>    
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
+Route::get('logout', 'Auth\LoginController@logout')->middleware('auth')->name('logout');
 
 Route::get('/admin-acara', function () {
     return view('admin.acara');
@@ -13,7 +14,3 @@ Route::get('/admin-acara', function () {
 Route::get('/mahasiswa-acara', function () {
     return view('mahasiswa.acara');
 })->middleware('auth')->name('mahasiswa-acara');
-
-Route::get('/admin-acara', function () {
-    return view('admin.acara');
-});
