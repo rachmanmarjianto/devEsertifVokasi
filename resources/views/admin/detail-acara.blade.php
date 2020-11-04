8c21923b295dddc58b09a3c6a3fad12a30fff5a0
deaamartya <50395247+deaamartya@users.noreply.github.com>
2020-11-04 08:16:37 +0700

    ganti type nim ke text

--- resources/views/admin/detail-acara.blade.php
+++ resources/views/admin/detail-acara.blade.php
@@ -183,7 +183,7 @@
                                 <tr id="peserta-{{ $loop->index }}">
                                     <td id="nomor-{{ $loop->index }}">{{ $loop->iteration }}</td>
                                     <td id="nim-{{ $loop->index }}">
-                                        <input class="form-control" type="number" name="nim[]" required value="{{ $d->nim }}" readonly>
+                                        <input class="form-control" type="text" name="nim[]" required value="{{ $d->nim }}" readonly>
                                     </td>
                                     <td id="nama-{{ $loop->index }}">
                                         <input class="form-control" type="text" name="nama[]" required value="{{ $d->user->NAMA_USER }}" readonly>
