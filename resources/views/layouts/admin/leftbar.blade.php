c0d9030ef1fd8d6b9f0b16c3457bfe0fc031c33a
dimas2jc <50562377+dimas2jc@users.noreply.github.com>
2020-11-04 10:09:22 +0700

    revisi logo unair

--- resources/views/layouts/admin/leftbar.blade.php
+++ resources/views/layouts/admin/leftbar.blade.php
@@ -4,20 +4,20 @@
         <!-- Start Navigationbar -->
         <div class="navigationbar">
             <div class="vertical-menu-icon">
-                <!-- <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
                     <div class="logobar">
-                        <a href="{{url('/')}}" class="logo logo-small"><img src="{{asset('assets/images/LogoUnair.png')}}" class="img-fluid" alt="logo"></a>
+                        <a href="{{url('/')}}" class="logo logo-small"><img src="{{asset('assets/images/logo-unair-baru.png')}}" class="img-fluid" style="height: 80%; width: 80%;" alt="logo"></a>
                     </div>
-                    <a class="nav-link active" id="v-pills-crm-tab" data-toggle="pill" href="#v-pills-crm" role="tab" aria-controls="v-pills-crm" aria-selected="true"><img src="{{asset('assets/images/svg-icon/crm.svg')}}" class="img-fluid" alt="CRM" data-toggle="tooltip" data-placement="top" title="CRM"></a>
+                    <!-- <a class="nav-link active" id="v-pills-crm-tab" data-toggle="pill" href="#v-pills-crm" role="tab" aria-controls="v-pills-crm" aria-selected="true"><img src="{{asset('assets/images/svg-icon/crm.svg')}}" class="img-fluid" alt="CRM" data-toggle="tooltip" data-placement="top" title="CRM"></a>
                     <a class="nav-link" id="v-pills-ecommerce-tab" data-toggle="pill" href="#v-pills-ecommerce" role="tab" aria-controls="v-pills-ecommerce" aria-selected="false"><img src="{{asset('assets/images/svg-icon/ecommerce.svg')}}" class="img-fluid" alt="eCommerce" data-toggle="tooltip" data-placement="top" title="eCommerce"></a>                            
                     <a class="nav-link" id="v-pills-hospital-tab" data-toggle="pill" href="#v-pills-hospital" role="tab" aria-controls="v-pills-hospital" aria-selected="false"><img src="{{asset('assets/images/svg-icon/hospital.svg')}}" class="img-fluid" alt="Hospital" data-toggle="tooltip" data-placement="top" title="Hospital"></a>
                     <a class="nav-link" id="v-pills-uikits-tab" data-toggle="pill" href="#v-pills-uikits" role="tab" aria-controls="v-pills-uikits" aria-selected="false"><img src="{{asset('assets/images/svg-icon/ui-kits.svg')}}" class="img-fluid" alt="UI Kits" data-toggle="tooltip" data-placement="top" title="UI Kits"></a>
-                    <a class="nav-link" id="v-pills-pages-tab" data-toggle="pill" href="#v-pills-pages" role="tab" aria-controls="v-pills-pages" aria-selected="false"><img src="{{asset('assets/images/svg-icon/pages.svg')}}" class="img-fluid" alt="Pages" data-toggle="tooltip" data-placement="top" title="Pages"></a>
-                </div> -->
+                    <a class="nav-link" id="v-pills-pages-tab" data-toggle="pill" href="#v-pills-pages" role="tab" aria-controls="v-pills-pages" aria-selected="false"><img src="{{asset('assets/images/svg-icon/pages.svg')}}" class="img-fluid" alt="Pages" data-toggle="tooltip" data-placement="top" title="Pages"></a> -->
+                </div>
             </div>
             <div class="vertical-menu-detail">
                 <div class="logobar">
-                    <a href="{{url('/')}}" class="logo"><img src="{{asset('/assets/images/LogoUnair.png')}}" class="img-fluid" style="height: 20%; width: 20%; margin-right: 2px" alt="logo"></a> FAKULTAS VOKASI
+                    FAKULTAS VOKASI
                 </div>
                 <div class="tab-content" id="v-pills-tabContent">
                     <div class="tab-pane fade show active" id="v-pills-crm" role="tabpanel" aria-labelledby="v-pills-crm-tab">
